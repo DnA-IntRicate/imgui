621870181b8362c8de6c790dc66dba899f4dd9e9
Adam Foflonker <87928802+DnA-IntRicate@users.noreply.github.com>
2025-09-25 12:48:15 +0200

    Dynamic linking

--- imconfig.h
+++ imconfig.h
@@ -27,6 +27,18 @@
 //#define IMGUI_API __declspec(dllimport)                   // MSVC Windows: DLL import
 //#define IMGUI_API __attribute__((visibility("default")))  // GCC/Clang: override visibility when set is hidden
 
+#ifdef IMGUI_SHARED
+    #ifdef IMGUI_BUILD
+        #define IMGUI_API __declspec(dllexport)
+    #else
+        #define IMGUI_API __declspec(dllimport)
+    #endif
+#endif
+
+#ifndef IMGUI_API
+    #define IMGUI_API
+#endif
+
 //---- Don't define obsolete functions/enums/behaviors. Consider enabling from time to time after updating to clean your code of obsolete function/names.
 //#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
 
